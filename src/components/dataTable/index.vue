3c4906c777c4289fa0bc086b8c8f369474487d67
wenzr <name_wzr@qq.com>
2018-02-26 13:24:25 +0800

    datatable

---
+++ src/components/dataTable/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="data-table">
+    <ul id="data-title">
+      <li v-for="item in titleList">
+        {{ item.name }}
+      </li>
+    </ul>
+    <ul id="data-list">
+      <li v-for="(item, index) in dataFullList">
+        <div :class="item.name" :style="{width: item.width}">
+          {{ item.value }}
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "index",
+      props: {
+        titleList: {
+          type: Array,
+          default: () => ([])
+        },
+        dataFullList: {
+          type: Array,
+          default: () => ([])
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
